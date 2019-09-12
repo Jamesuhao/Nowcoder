9bcd43e5147f1ef24a9dd0e2769de35020ddd2d0
苏豪 <1766727840@qq.com>
2019-09-12 09:31:43 +0800

    c语言细节问题

--- C语言——选择填空/C语言——选择填空/test.c
+++ C语言——选择填空/C语言——选择填空/test.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#if 0
 //1.在c语言中，无符号整型在与有符号进行运算时，统一当做无符号处理
 int main()
 {
@@ -59,3 +60,55 @@ int main()
 	printf("%d %d\n", x, i);
 	return 0;
 }
+
+//5.double打印小数点跟后6位
+int main()
+{
+	double x = 2.0 ;
+	double y = 0.0;
+	if (x < 0.0)
+	{
+		y = 0.0;
+	}
+	else if ((x < 5.0) && (!x))//(!x)表示只有x为0时表达式为真
+	{
+		y = 1.0 / (x + 2.0);
+	}
+	if (x < 10.0)
+	{
+		y = 1.0 / x;
+	}
+	else
+	{
+		y = 10.0;
+	}
+	printf("%f\n", y);
+	return 0;
+}
+
+//6注意区分表达式和赋值的区别，表达式之关心真假
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int m = 6;
+	int n = 4;
+	int *p = &n;
+	int *q = &m;
+	i = p == &m;
+	j = (-*p) / (*q) + 7;
+	printf("i=%d,j=%d\n", i, j);
+	return 0;
+}
+//7枚举的特性：enum中：首元素不赋值的话，默认为0；后一个元素不赋值的话比前一个元素大1
+enum {
+	a, b = 5, c, d = 4, e
+} k;
+int main()
+{
+	k = c;
+	printf("k=%d\n", k);
+	return 0;
+}
+#endif
+
